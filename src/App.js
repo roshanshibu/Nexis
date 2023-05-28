bb6d2119f6685f08b5be3b711cfbf00220384567
Roshan Shibu <roshanshibu97@gmail.com>
2023-05-28 21:32:26 +0200

    reorganize code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import React from "react";
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
 import "./App.css";
-
-const srhMarkerIcon = new Icon({
-  iconUrl: require("./images/SRH_Icon.png"),
-  iconSize: [45, 38]
-})
+import Home from "./Home/Home";
 
 function App() {
-  return (<MapContainer center={[49.4043, 8.6758]} zoom={14}>
-    <TileLayer
-      attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-      url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-      // url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
-      // url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-      // url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-      // url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png"
-      
-      />
-    <Marker position={[49.4139, 8.6511]} icon={srhMarkerIcon}
-      data={{key: "value"}}
-      eventHandlers={{
-        click: (e) => {
-          console.log(e.target.options.data);  // console log contents of data
-        },
-      }}>
-      <Popup><p>I go here!</p></Popup>
-    </Marker>
-  </MapContainer>);
+  return (<Home />)
 }
 
 export default App;
